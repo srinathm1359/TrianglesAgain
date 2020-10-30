3d95f225a27509fbdd2deee683cf19466615305e
Srinath Mahankali <smahankali10@stuy.edu>
2020-10-30 05:01:30 -0400

    Tested equals()

--- Tester.java
+++ Tester.java
@@ -1,11 +1,16 @@
 public class Tester {
   public static void main(String[] args) {
-    Point A = new Point(1,2);
     //Testing getX() and getY()
+    Point A = new Point(1,2);
     System.out.println(A.getX());
     System.out.println(A.getY());
-    Point B = new Point(4,6);
     //Testing distanceTo()
+    Point B = new Point(4,6);
     System.out.println(A.distanceTo(B));
+    //Testing equals()
+    Point C = new Point(1,2);
+    System.out.println(A.equals(B));
+    System.out.println(A.equals(C));
+    System.out.println(B.equals(C));
   }
 }
