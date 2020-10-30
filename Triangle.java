f011553e1e8f8d9b2cfb5ea7529642d21820fe25
Srinath Mahankali <smahankali10@stuy.edu>
2020-10-30 05:12:24 -0400

    Added Triangle() with coordinates

--- Triangle.java
+++ Triangle.java
@@ -6,5 +6,10 @@ public class Triangle {
     v2 = b;
     v3 = c;
   }
-  
+
+  public Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
+    v1 = new Point(x1,y1);
+    v2 = new Point(x2,y2);
+    v3 = new Point(x3,y3);
+  }
 }
