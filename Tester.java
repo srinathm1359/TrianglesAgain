2ead36ced7fecad9c2055250deb62af6f9b6fdba
Srinath Mahankali <smahankali10@stuy.edu>
2020-10-30 04:52:25 -0400

    Tested getY()

--- Tester.java
+++ Tester.java
@@ -1,6 +1,8 @@
 public class Tester {
   public static void main(String[] args) {
     Point A = new Point(1,2);
+    //Testing getX and getY
     System.out.println(A.getX());
+    System.out.println(A.getY());
   }
 }
