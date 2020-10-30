a229ef969ead8fde84f647a704b6e6a4b44bb953
Srinath Mahankali <smahankali10@stuy.edu>
2020-10-30 04:58:32 -0400

    Tested distanceTo()

--- Tester.java
+++ Tester.java
@@ -1,8 +1,11 @@
 public class Tester {
   public static void main(String[] args) {
     Point A = new Point(1,2);
-    //Testing getX and getY
+    //Testing getX() and getY()
     System.out.println(A.getX());
     System.out.println(A.getY());
+    Point B = new Point(4,6);
+    //Testing distanceTo()
+    System.out.println(A.distanceTo(B));
   }
 }
